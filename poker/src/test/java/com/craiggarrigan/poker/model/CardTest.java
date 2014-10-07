82d91a55bb6eb8c36645d30f4b45e0e51f6699b3
Craig Garrigan <craiggarrigan@gmail.com>
2014-10-07 23:31:01 +0100

    Added equality behaviour to Card class

--- poker/src/test/java/com/craiggarrigan/poker/model/CardTest.java
+++ poker/src/test/java/com/craiggarrigan/poker/model/CardTest.java
@@ -38,5 +38,32 @@ public class CardTest {
 			}
 		}
 	}
+	
+	@Test
+	public void shouldOverrideEquals(){
+		Card aceSpades1 = new Card(Rank.ACE, Suit.SPADES);
+		Card aceSpades2 = new Card(Rank.ACE, Suit.SPADES);
+		Card sevenHearts = new Card(Rank.SEVEN, Suit.HEARTS);
+		// It is reflexive: for any non-null reference value x, x.equals(x) should return true.
+		assertTrue(aceSpades1.equals(aceSpades1));
+		// It is symmetric: for any non-null reference values x and y, x.equals(y) should return true if and only if y.equals(x) returns true.
+		assertTrue(aceSpades1.equals(aceSpades2));
+		assertTrue(aceSpades2.equals(aceSpades1));
+		assertFalse(aceSpades1.equals(sevenHearts));
+		assertFalse(sevenHearts.equals(aceSpades1));
+		// For any non-null reference value x, x.equals(null) should return false.
+		assertFalse(aceSpades1.equals(null));
+	}
+	
+	@Test
+	public void shouldObeyHashCodeContract(){
+		Card aceSpades1 = new Card(Rank.ACE, Suit.SPADES);
+		Card aceSpades2 = new Card(Rank.ACE, Suit.SPADES);		
+		// Whenever it is invoked on the same object more than once during an execution of a Java application, the hashCode method must consistently return the same integer, provided no information used in equals comparisons on the object is modified. 
+		int hashCode = aceSpades1.hashCode();
+		assertEquals(hashCode, aceSpades1.hashCode());
+		// If two objects are equal according to the equals(Object) method, then calling the hashCode method on each of the two objects must produce the same integer result.
+		assertEquals(aceSpades1.hashCode(), aceSpades2.hashCode());
+	}
 
 }
